4e921267f7affe9a890ce49ff17a035faf89ae4a
TopherBsk <jua.silva@duocuc.cl>
2024-09-25 20:01:49 -0300

    Creacion conductor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'conductor',
+    loadChildren: () => import('./pages/conductor/conductor.module').then( m => m.ConductorPageModule)
+  },
 ];
 
 @NgModule({
