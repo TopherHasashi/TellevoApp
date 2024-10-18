9fbc1c5492c5b5209b2948e85243e1067d1c9084
CETECOM <cetecom_conce@duoc.cl>
2024-10-18 13:10:15 -0300

    localstorage basico y logout

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email: string = '';   
+  password: string = '';  
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  login() {
+    if (this.email && this.password) {
+      // Guarda los datos del usuario en LocalStorage
+      localStorage.setItem('user', JSON.stringify({ email: this.email, password: this.password }));
+
+      // Redirige a la página principal (por ejemplo, /home)
+      this.navCtrl.navigateForward('/home');
+    } else {
+      // Mensaje de error si los campos están vacíos
+      alert('Por favor, ingresa tu correo y contraseña');
+    }
   }
-
 }
+
