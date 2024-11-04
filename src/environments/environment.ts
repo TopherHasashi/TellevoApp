c8bb9158ab6ba1d82416ab79839660410aa05548
Sebamediina <seba.medina.net@gmail.com>
2024-11-04 12:53:04 -0300

    Integracion de Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAXLG5Ai3ktQ_jxhD1lpNR0TREmhWhYMFc",
+    authDomain: "tellevoapp-e8217.firebaseapp.com",
+    projectId: "tellevoapp-e8217",
+    storageBucket: "tellevoapp-e8217.firebasestorage.app",
+    messagingSenderId: "104776425993",
+    appId: "1:104776425993:web:bda6b953879a1415308ed8",
+    measurementId: "G-Y6XEED4MR3"
+  }
 };
 
 /*
