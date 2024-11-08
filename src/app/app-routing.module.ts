3854a735518ec8eeb5b3aa71bd0796836780da58
TopherBsk <jua.silva@duocuc.cl>
2024-11-08 13:32:08 -0300

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,11 @@ const routes: Routes = [
   {
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },  {
+    path: 'registro',
+    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
   },
+
 
 ];
 
